ce53b205f7824040276a32801a9fe19375295e40
Zak Soliman <zakaria.soliman1@gmail.com>
2023-12-20 15:08:47 -0500

    Made list iterable

--- src/second.rs
+++ src/second.rs
@@ -72,6 +72,19 @@ impl<T> Drop for List<T> {
     }
 }
 
+// SECTION: Iteration
+// struct ListIter<T> {
+//     list: &List<T>,
+// }
+
+impl<T> Iterator for List<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.pop()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::List;
@@ -131,5 +144,18 @@ mod test {
 
         assert_eq!(list.peek(), Some(&42));
         assert_eq!(list.pop(), Some(42));
+    }
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        // let mut iter = list.into_iter();
+        assert_eq!(list.next(), Some(3));
+        assert_eq!(list.next(), Some(2));
+        assert_eq!(list.next(), Some(1));
+        assert_eq!(list.next(), None);
     }
 }
